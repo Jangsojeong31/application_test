54e3c4dce23689957cbca577f6f4882298b03dea
Jangsojeong31 <sojung301@naver.com>
2025-05-16 10:14:58 +0900

    깃 연결

---
+++ src/main/java/com/study/application_test/ApplicationTestApplication.java
@@ -0,0 +1,13 @@
+package com.study.application_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/application_test/ApplicationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.application_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
